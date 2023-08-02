79da6a47610860e7ba6a8200bf208664c5e89587
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-08-03 01:10:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my_jaspr_app
+
+A basic pure-dart web app with ssr & automatic client hydration.
---
+++ web/app.client.dart
@@ -0,0 +1,13 @@
+// GENERATED FILE, DO NOT MODIFY
+// Generated with jaspr_builder
+
+import 'package:jaspr/browser.dart';
+import 'package:my_jaspr_app/app.dart' as a;
+
+void main() {
+  runAppWithParams(getComponentForParams);
+}
+
+Component getComponentForParams(ConfigParams p) {
+  return a.App();
+}
